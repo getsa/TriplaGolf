cb0fe6446835500671ca30c646cf12449f4aa1e0
getsa <miikka84@hotmail.com>
2019-12-22 15:10:19 +0200

    insta lisätty

--- funs/settings.js
+++ funs/settings.js
@@ -1,42 +1,77 @@
 function settingsScreen() {
   console.log("Näytöllä: settingsScreen ");
-  console.log(G_game.maxStrokes);
   G_myTeam.players.length > 0 ? $('#NavBtnJatka').show() : $('#NavBtnJatka').hide();
 
 
   $("#gameAppDiv").empty();
   $("#infoDiv").hide();
   //Main elements
-
-  // Tähän lajikohtaiset maximimäärät ja pistemäärät päivittymään!
   $("#gameAppDiv").append(`
     <div class="w3-container">
-      <h3> Ei toimi vielä... </h3>
-      <p> Max. tulos = + <input id="maxStrokesID" class="settingInput" type="number" value="${G_game.maxStrokes}"></input></p>
-      <hr>
-      <p> Pistejako:</p>
-      <p> Pistesijojen määrä: <input id="points1" value="12" class="settingInput" type="number"> </p>
-      <ol>
-        <li><input id="points1" value="14" class="settingInput" type="number"></input></li>
-        <li><input id="points2" value="12"class="settingInput" type="number"></input></li>
-        <li><input id="points3" value="10"class="settingInput" type="number"></input></li>
-        <li> <input id="points4" value="9"class="settingInput" type="number"></input></li>
-        <li><input id="points5" value="8"class="settingInput" type="number"></input></li>
-        <li><input id="points6" value="7" class="settingInput" type="number"></input></li>
-        <li><input id="points7" value="6"class="settingInput" type="number"></input></li>
-        <li> <input id="points8" value="5" class="settingInput" type="number"></input></li>
-        <li> <input id="points9" value="4" class="settingInput" type="number"></input></li>
-        <li> <input id="points10" value="3" class="settingInput" type="number"></input></li>
-        <li> <input id="points11" value="2" class="settingInput" type="number"></input></li>
-        <li> <input id="points12" value="1" class="settingInput" type="number" value></input></li>
-      </ol>
-      <hr>
-        <button id="clearLocaleStorageBtn" class="w3-btn w3-green"> Tyhjennä lokaalimuisti </button>
+      <br>
+      <button id="gameSettingsBtn" class="w3-btn w3-green"> Peliasetukset </button><br><br>
+      <button id="debugBtn" class="w3-btn w3-green"> Säätö </button><br><br>
     </div>
     `);
-  $("#clearLocaleStorageBtn").click(()=>{
-    localStorage.clear();
-  });
+
+  $("#gameSettingsBtn").click( () => gameSettingsScreen() );
+  $("#debugBtn").click( () => debugSettingsScreen() );
+
+  function gameSettingsScreen() {
+    $("#gameAppDiv").empty();
+    $("#gameAppDiv").append(`
+      <div class="w3-container">
+        <br>
+        <button id="maxSrokesBtn" class="w3-btn w3-green"> Maksimi PAR ylitys </button><br><br>
+        <button id="pointsArrayBtn" class="w3-btn w3-green"> Pistejako </button><br><br>
+      </div>
+      `);
+
+      $("#maxSrokesBtn").click( () => maxStrokesScreen() );
+      $("#pointsArrayBtn").click( () => pointsArrayScreen() );
+
+      function maxStrokesScreen() {
+        $("#gameAppDiv").empty();
+      }
+      function pointsArrayScreen() {
+        $("#gameAppDiv").empty();
+      }
+
+  }
+
+  function debugSettingsScreen() {
+    $("#gameAppDiv").empty();
+  }
+  
+  // Tähän lajikohtaiset maximimäärät ja pistemäärät päivittymään!
+  // $("#gameAppDiv").append(`
+  //   <div class="w3-container">
+  //     <h3> Ei toimi vielä... </h3>
+  //     <p> Max. tulos = + <input id="maxStrokesID" class="settingInput" type="number" value="${G_game.maxStrokes}"></input></p>
+  //     <hr>
+  //     <p> Pistejako:</p>
+  //     <p> Pistesijojen määrä: <input id="points1" value="12" class="settingInput" type="number"> </p>
+  //     <ol>
+  //       <li><input id="points1" value="14" class="settingInput" type="number"></input></li>
+  //       <li><input id="points2" value="12"class="settingInput" type="number"></input></li>
+  //       <li><input id="points3" value="10"class="settingInput" type="number"></input></li>
+  //       <li> <input id="points4" value="9"class="settingInput" type="number"></input></li>
+  //       <li><input id="points5" value="8"class="settingInput" type="number"></input></li>
+  //       <li><input id="points6" value="7" class="settingInput" type="number"></input></li>
+  //       <li><input id="points7" value="6"class="settingInput" type="number"></input></li>
+  //       <li> <input id="points8" value="5" class="settingInput" type="number"></input></li>
+  //       <li> <input id="points9" value="4" class="settingInput" type="number"></input></li>
+  //       <li> <input id="points10" value="3" class="settingInput" type="number"></input></li>
+  //       <li> <input id="points11" value="2" class="settingInput" type="number"></input></li>
+  //       <li> <input id="points12" value="1" class="settingInput" type="number" value></input></li>
+  //     </ol>
+  //     <hr>
+  //       <button id="clearLocaleStorageBtn" class="w3-btn w3-green"> Tyhjennä lokaalimuisti </button>
+  //   </div>
+  //   `);
+  // $("#clearLocaleStorageBtn").click(()=>{
+  //   localStorage.clear();
+  // });
 
   $("#maxStrokesID").change( () => {
     num = parseInt($("#maxStrokesID").val(), 10);
